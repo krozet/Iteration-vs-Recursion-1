2fa38ff273b11475c01414f10d08804912177345
krozet <keawa.rozet@yahoo.com>
2018-03-01 14:51:08 -0800

    KR03 - Added a way to measure how long each method takes to perform its search. Also fixed infinite while loop bug in bsI.

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
-#include <cstring>
+#include <limits>
 
-#define SIZE 100
+#define SIZE 89999
+#define K 300
+
+typedef std::numeric_limits< double > dbl;
 
 void initArray(int* array);
 void printArray(int* array);
 int binarySearchIteration(int* array, int n, int i);
 int binarySearchRecursion(int* array, int left, int n, int i);
+void bsTimeMeasurement(int* array, int n);
 
 int main(int argc, char const** argv) {
   int array[SIZE];
   initArray(array);
-  printf("Iteration: %d\tRecursion: %d\n", binarySearchIteration(array, SIZE, 10),
-                                           binarySearchRecursion(array, 0, SIZE, 10));
-  // binarySearchIteration(array, SIZE, 10);
-  // binarySearchRecursion(array, 0, SIZE, 10);
+  bsTimeMeasurement(array, SIZE);
+
   return 0;
 }
 
@@ -33,7 +34,7 @@ int binarySearchIteration(int* array, int n, int i) {
       left = mid + 1;
     //value is greater than the middle
     else
-      n = mid + 1;
+      n = mid - 1;
   }
   //element not found
   return -1;
@@ -41,7 +42,7 @@ int binarySearchIteration(int* array, int n, int i) {
 
 int binarySearchRecursion(int* array, int left, int n, int i) {
   int mid = left + (n - left)/2;
-  if (n > 0) {
+  if (n >= left) {
     //value in the middle
     if (array[mid] == i)
       return mid;
@@ -57,8 +58,8 @@ int binarySearchRecursion(int* array, int left, int n, int i) {
 
 void initArray(int* array) {
   int rd;
-  array[0] = 1;
-  for (int i = 1; i < SIZE; i++) {
+  // array[0] = 1;
+  for (int i = 0; i < SIZE; i++) {
     //ascending sorted array
     array[i] = i;
     //random sorted array
@@ -71,3 +72,26 @@ void printArray(int* array) {
     printf("%d\n", array[i]);
   }
 }
+
+void bsTimeMeasurement(int* array, int n) {
+  int j, m;
+  //first test binarySearchIteration
+  clock_t begin = clock();
+  for(j = 0; j < K; j++)
+    for(m = 0; m < n; m++)
+      binarySearchIteration(array, n, m);
+  clock_t end = clock();
+  double bsI_elapsed_secs = double(end - begin)/CLOCKS_PER_SEC;
+  std::cout.precision(dbl::max_digits10);
+  std::cout << "Binary Search Iteration at K= " << K <<": " << std::fixed << bsI_elapsed_secs << " seconds." << std::endl;
+
+  //second test binarySearchRecursion
+  clock_t begin2 = clock();
+  for(j = 0; j < K; j++)
+      for(m = 0; m < n; m++)
+          binarySearchRecursion(array, 0, n, m);
+  clock_t end2 = clock();
+  double bsR_elapsed_secs = double(end2 - begin2)/CLOCKS_PER_SEC;
+  std::cout.precision(dbl::max_digits10);
+  std::cout << "Binary Search Recursion at K= " << K <<": " << std::fixed << bsR_elapsed_secs << " seconds." << std::endl;
+  }
